2595d9a250aa810a5146e95623e3ef6c81457dfc
radiumSodium <95066025+radiumSodium@users.noreply.github.com>
2022-03-23 02:07:37 +0600

    Add files via upload

---
+++ HomeWork-SIRANA/oddnumbetToN.c
@@ -0,0 +1,26 @@
+// ------------------------------------ SI RANA ----------------------------
+#include <stdio.h>
+
+int main()
+{
+    printf("-------------- Odd number up to N ----------------- \n");
+    int i, n;
+    
+    
+    printf("Print odd numbers till: ");
+    scanf("%d", &n);
+
+    printf("All odd numbers from 1 to %d are: \n", n);
+
+    
+    for(i=1; i<=n; i++)
+    {
+        
+        if(i%2!=0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
